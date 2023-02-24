f22ec99fb6d670b36cbeb8cea69fbc014a35a97e
Boris Maximenko <bbmaxi@mail.ru>
2023-02-24 18:47:51 +0300

    24.02.23

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import './index.html';
 import './index.scss';
-import code from './img/code.png'
-import { mult, sum } from './modules/calc';
+import './scripts/main.js';
+
---
+++ src/scripts/navigation.js
@@ -0,0 +1,24 @@
+const toggleNavigation = () => {
+    const toggleButton = document.querySelector(".header__burger");
+    const nav = document.querySelector(".navigation");
+
+    toggleButton.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const isClose = toggleButton.classList.contains("header__burger--close");
+
+        if (isClose) {
+            toggleButton.classList.remove("header__burger--close");
+            toggleButton.classList.add("header__burger--open");
+            nav.classList.remove("navigation--close");
+        } else {
+            toggleButton.classList.add("header__burger--close");
+            toggleButton.classList.remove("header__burger--open");
+            nav.classList.add("navigation--close");
+        }
+
+    }) 
+    
+};
+
+toggleNavigation();
