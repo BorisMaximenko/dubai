bc24d49b8fcf2fbe7cb524472bd176285ca3dfcd
Boris Maximenko <bbmaxi@mail.ru>
2023-02-27 17:55:17 +0300

    last

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,10 +2,12 @@ const toggleDropdown = () => {
     const activator = document.querySelector(".menu__link--drop-down-activator");
     const dropdown = document.querySelector(".menu__drop-down-list");
 
-    
+
     activator.addEventListener('click', (event) =>{
         event.preventDefault();
+
         const isOpen = activator.classList.contains("menu__link--active");
+        
         if (isOpen) {
             activator.classList.remove("menu__link--active");
             dropdown.classList.add("menu__drop-down-list--close");
@@ -17,3 +19,4 @@ const toggleDropdown = () => {
 };
 
 toggleDropdown();
+
